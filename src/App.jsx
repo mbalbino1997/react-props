cd62416a3e753edff542021f68688f50a43a5889
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-11-27 17:32:23 +0100

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Button from "./components/ui/button/button.jsx"
 import Header from "./components/header/header.jsx"
 import Main from "./components/main/main.jsx"
+import Footer from "./components/footer/footer.jsx"
 export default function App() {
 
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <section>
         <Main />
       </section>
+      <Footer />
 
     </>
   )
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,9 @@
+import style from "./footer.module.css"
+
+export default function () {
+    return (
+        <footer>
+            <p className={style.footerText}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati sunt ratione, voluptatibus ea deleniti sint dignissimos dicta odio magni amet et laudantium, iure id voluptatem, assumenda dolores? Culpa at cupiditate, inventore laborum suscipit possimus dolorum consectetur laudantium commodi, maxime, modi fugiat odit nesciunt a nobis laboriosam eius. Iure, doloribus vero!</p>
+        </footer>
+    )
+}
