8d0c902059d6accd7917c510f0b5ec35050472bc
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-11-28 16:43:42 +0100

    add

--- src/components/ui/card/card.jsx
+++ src/components/ui/card/card.jsx
@@ -4,7 +4,7 @@ import placeholder from "../../../assets/imgs/placeholder.jpg"
 export default function ({ title = "", image, content = "" }) {
     return (
         <div className={style.card}>
-            <figure className={style.card_figure}><img src={image ?? placeholder} alt="" /></figure>
+            <figure className={style.card_figure}><img className={style.card_img} src={image ?? placeholder} alt="" /></figure>
             <div className={style.card_description}>
                 <h3>{title}</h3>
                 <p>{content}</p>
